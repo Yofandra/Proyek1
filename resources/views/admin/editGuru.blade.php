53b68986d710c034197e821c492eeabc79d24643
auliaacahyaa <auliabrilianaa@gmail.com>
2023-05-17 13:00:21 +0700

    editGuru

--- resources/views/admin/editGuru.blade.php
+++ resources/views/admin/editGuru.blade.php
@@ -1,62 +1,5 @@
 @extends('admin.layoutAdmin')
-<<<<<<< HEAD
 
-@section('content')
-
-<div class="container mt-5">
-
-    <div class="row justify-content-center align-items-center">
-        <div class="card" style="width: 24rem;">
-            <div class="card-header">
-            Edit Guru
-            </div>
-        <div class="card-body">
-            @if ($errors->any())
-            <div class="alert alert-danger">
-                <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
-        <form method="post" action="{{ route('guru.update', $Guru->nip) }}" id="myForm">
-        @csrf
-        @method('PUT')
-            <div class="form-group">
-                <label for="nip">NIP</label>
-                <input type="text" name="nip" class="formcontrol" id="nip" value="{{ $Guru->nip }}" ariadescribedby="nip" >
-            </div>
-            <div class="form-group">
-                <label for="nama">Nama</label>
-                <input type="text" name="nama" class="formcontrol" id="nama" value="{{ $Guru->nama_guru }}" ariadescribedby="nama" >
-            </div>
-            <div class="form-group">
-                <label for="kelas">Kelas</label>
-                <input type="kelas" name="kelas" class="formcontrol" id="kelas" value="{{ $Guru->kelas }}" ariadescribedby="kelas" >
-            </div>
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" name="username" class="formcontrol" id="username" value="{{ $Guru->username }}" ariadescribedby="username" >
-            </div>
-            <div class="form-group">
-                <label for="Jurusan">Password</label>
-                <input type="Jurusan" name="Jurusan" class="formcontrol" id="Jurusan" value="{{ $Guru->Jurusan }}" ariadescribedby="Jurusan" >
-            </div>
-            <div class="form-group">
-                <label for="No_Handphone">No_Handphone</label>
-                <input type="No_Handphone" name="No_Handphone" class="formcontrol" id="No_Handphone" value="{{ $Guru->No_Handphone }}" ariadescribedby="No_Handphone" >
-                </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-            </div>
-        </div>
-    </div>
-</div>
-=======
 @section('content')
 
     <div class="flex flex-col items-center pb-10">
@@ -103,5 +46,4 @@
             </div>
         </div>
     </div>
->>>>>>> 6fcbc44b16ed35d531d275469dc5d6ecc5cac339
 @endsection
