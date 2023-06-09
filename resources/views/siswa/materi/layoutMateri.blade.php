fc88062b342a06a2e0407fc71e86c004d115279a
= RizqiRHuda <= rizqirhuda45@gmail.com>
2023-06-09 07:33:32 +0700

    edit layout materi

--- resources/views/siswa/materi/layoutMateri.blade.php
+++ resources/views/siswa/materi/layoutMateri.blade.php
@@ -1,7 +1,7 @@
 @extends('siswa.layoutSiswa')
 @section('content')
 
-<div class="flex flex-wrap -mx-6">
+<div class="flex flex-wrap">
     <div class="sm:w-1/2 xl:w-3/4">
         <div class="flex items-center px-5 py-6 bg-white">
             @yield('contentMateri')
